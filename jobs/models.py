376f9c8901800f38541ebe50fb9ea591e5e187d9
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-06-23 00:51:42 -0500

    added details of project

--- jobs/models.py
+++ jobs/models.py
@@ -10,3 +10,6 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     # summary
     summary = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.summary
