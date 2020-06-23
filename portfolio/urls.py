9197e1aa0c22664e26f2af384d55835def9f3d71
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-06-22 19:06:38 -0500

    new url path created for my name

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('viviana', jobs.views.viviana, name='viviana'),
 ]
