4def1ff8c5b1aaff6105889d319ab29deaec1c64
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-06-22 19:34:12 -0500

    class job created

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# images
+
+
+class Job(models.Model):
+    # Images -accpept jpg
+    # you need to install: pip3 install pillow
+    image = models.ImageField(upload_to='images/')
+    # summary
+    summary = models.CharField(max_length=200)
